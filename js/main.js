8c34466061e44fb310cedf19dd0b104140eb78d3
Springship981 <121887158+Springship981@users.noreply.github.com>
2023-10-09 18:44:46 +0100

    Fixed project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active')
+}
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
